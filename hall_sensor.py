9a6371eb8245a4eb3ac4201a5bc6c9f1ffc6e050
Mark Nicholls <mark@nichollsclan.ca>
2022-09-14 14:09:42 -0400

    moved to using mqtt, moved secrets out to secrets.py
    Sends data to mqtt instead of influxdb directly now.
    Secrets moved out to separate secrets.py file.

--- hall_sensor.py
+++ hall_sensor.py
@@ -1,16 +1,24 @@
 from machine import ADC, Pin
 from utime import sleep
 from math import sqrt
-
 import network
 import socket
 import time
-import urequests
+import ubinascii
+from umqtt.simple import MQTTClient
+import secrets
 
-#Endpoint of your influxdb, currently plaintext and unauthenticated
-influx_url = "http://192.168.10.24:8086/write?db=george"  
-ssid = 'SSID'  #Wireless SSID
-password = 'SSIDPASSWORD' #Wireless SSID Password
+
+# Default MQTT server to connect to
+mqtt_server = secrets.mqtt_server 
+mqtt_user = secrets.mqtt_user
+mqtt_pass = secrets.mqtt_password
+client_id = secrets.client_id
+print(client_id)
+TOPIC = "george-o-meter/{0}".format(client_id)
+
+ssid = secrets.ssid  #Wireless SSIS
+password = secrets.ssid_pass #Wireless SSID Password
 
 #Turn on the onboard LED 
 led = machine.Pin("LED", machine.Pin.OUT)
@@ -42,6 +50,16 @@ else:
 
 hall_sensor = ADC(26)
 
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, keepalive=3600, user=mqtt_user, password=mqtt_pass)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    return client
+
+def reconnect():
+    print('Failed to connect to the MQTT Broker. Reconnecting...')
+    time.sleep(5)
+    machine.reset()
 
 global last_reading
 last_reading = 3.3  ## Record last to make sure wheel is going around and not just staying there.
@@ -56,7 +74,8 @@ def batch_read():
     global counter
     global last_reading
     i = 0
-    while i < 20:
+    #This tries to approximate a 10 second period.  Its probably not real accurate.
+    while i < 100:
         current_reading = read_sensor()
         current_diff = last_reading - current_reading
         if current_diff > 2:
@@ -69,18 +88,23 @@ def batch_read():
 def read_sensor():
     reading = hall_sensor.read_u16() * conversion_factor
     return (reading)
-
+try:
+    client = mqtt_connect()
+except OSError as e:
+    reconnect()
 while True:
     try:
         counts = batch_read()
     except:
         print("Error occurred during batch read")
     try:    
-        data_string = "Counter count={0}".format(counter)
+        data_string = b'{{"rotations":{0}}}'.format(counter)
         print(data_string)
-        response = urequests.post(influx_url, data=data_string)
-        print(response.text)
+        client.publish(TOPIC, data_string)
+        #reset counter on successful publish
         counter = 0
     except:
-        print("influxdb error")
-    sleep(0.2)
+        print("error")
+    sleep(0.1)    
+        
+
